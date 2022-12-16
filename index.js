dfc86926f329ff81253332e120c89b24df3a8326
Santos <santiagoamola@gmail.com>
2022-12-16 11:19:30 -0300

    Nuevo repositorio

---
+++ index.js
@@ -0,0 +1,58 @@
+fetch("js/listaItems.json") // Lista de cultivos en formato JSON
+    .then((resp) => resp.json())
+    .then(function (data) {
+        localStorage.setItem("itemsRunescape", JSON.stringify(data)); // Se guarda en local storage
+    });
+let individuoActual = 0;
+let misRegistros = localStorage.getItem("misRegistros") ? JSON.parse(localStorage.getItem("misRegistros")) : [];
+
+function CrearRegistro() {
+    misRegistros.push([misRegistros[misRegistros.length - 1] ? misRegistros[misRegistros.length - 1][0] + 1 : 1, "", 0, undefined, []]);
+    individuoActual = misRegistros.length - 1;
+    localStorage.setItem("individuoActual", JSON.stringify(individuoActual));
+    localStorage.setItem("misRegistros", JSON.stringify(misRegistros));
+    window.location.assign("pages/crear.html");
+}
+
+function borrarTodo() {
+    swal.fire({
+        title: '¿Está seguro?',
+        text: "No podrá deshacer esta acción",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sí, eliminar todo',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire(
+                'Borrado',
+                'Se eliminaron todos los datos',
+                'success'
+            )
+            window.localStorage.removeItem("misRegistros");
+            misRegistros = [];
+        }
+    })
+}
+
+
+let verMisRegistros = () => {
+    misRegistros[0] ? window.location.assign("pages/misregistros.html") :
+    Swal.fire(
+        'Mmm...',
+        'No hay registros creados',
+        'error'
+    )
+}
+
+document.getElementById("btnCrearRegistro").onclick = CrearRegistro;
+document.getElementById("verMisRegistros").onclick = verMisRegistros;
+document.getElementById("borrarTodo").onclick = borrarTodo;
+
+document.getElementById("info").onclick = () => {
+    location.assign("pages/info.html");
+}
+
+
